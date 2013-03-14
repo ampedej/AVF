57b07e928dd508fa676d34182420b2f296eae4a1
Edward M murray Jr <ampedej@fullsail.edu>
2013-03-14 19:22:38 -0400

    Changed folder names. Added some twiiter search data and styling.

---
+++ AppiOS/www/js/main.js
@@ -0,0 +1,21 @@
+/*
+Project 4
+Edward M Murray Jr
+ASD 1301
+*/
+//
+$(function twitData(){
+	$.getJSON("http://search.twitter.com/search.json?q=%23DuckDynasty&rpp=5&include_entities=true&result_type=popular&callback=?",
+	function(data){
+		console.log(data);
+		for(i=0, j=data.results.length;i<j; i++){
+			$('#twitter-data').append(
+				"<li>" + "<p>" + "<img src='" + data.results[i].profile_image_url + "'/><br />" + data.results[i].text + "<br />" + "<em>" + data.results[i].created_at + "</em>" + "</p>" + "</li>"
+			);
+		}
+	}
+	);
+	
+	var twitterLink = $('twitterLink');
+	twitterLink.on("click", twitData);
+});
