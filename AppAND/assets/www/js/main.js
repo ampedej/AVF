714373e37428305de1741030fac00cd9b1fcb0e4
Edward M murray Jr <ampedej@fullsail.edu>
2013-03-28 18:22:03 -0400

    Completed two mashup's for my final demo.

--- AppAND/assets/www/js/main.js
+++ AppAND/assets/www/js/main.js
@@ -4,6 +4,23 @@ Edward M Murray Jr
 AVF 1303
 */
 //
+
+//Device properties function
+function checkConnection() {
+        var networkState = navigator.network.connection.type;
+
+        var states = {};
+        states[Connection.UNKNOWN]  = 'Unknown connection';
+        states[Connection.ETHERNET] = 'Ethernet connection';
+        states[Connection.WIFI]     = 'WiFi connection';
+        states[Connection.CELL_2G]  = 'Cell 2G connection';
+        states[Connection.CELL_3G]  = 'Cell 3G connection';
+        states[Connection.CELL_4G]  = 'Cell 4G connection';
+        states[Connection.NONE]     = 'No network connection';
+
+       $('#twitresults').append( device.name + " found these results via " + states[networkState]);
+    }
+
 $(function(){
 	$.getJSON("http://search.twitter.com/search.json?q=%23DuckDynasty&rpp=5&include_entities=true&result_type=popular&callback=?",
 	function(data){
--- AppiOS/www/js/main2.js
+++ AppiOS/www/js/main2.js
@@ -4,12 +4,25 @@ Edward M Murray Jr
 AVF 1303
 */
 //
-$(function(){
-	$.ajax({
-  url : "http://api.wunderground.com/api/cbfd448ed7906dea/geolookup/conditions/q/FL/Orlando.json",
-  dataType : "jsonp",
-  success : function(data) {
-  console.log(data);
+document.addEventListener("deviceready", onDeviceReady, false);
+
+// Cordova is ready
+function onDeviceReady() {
+	navigator.geolocation.getCurrentPosition(onSuccess, onError);
+}
+
+// onSuccess Run Geo Function
+function onSuccess(position) {
+	var element = document.getElementById('staticmap');
+    var lat = position.coords.latitude;
+    var lon = position.coords.longitude;
+    var pos = lat + "," + lon;//device coords for url 
+    var imsrc ="http://maps.googleapis.com/maps/api/staticmap?center=" + pos + "&zoom=18&size=600x600&maptype=roadmap&markers=color:red%7Clabel:A%7C" + pos + "&sensor=false&key=AIzaSyCJTZgUnB6kpfOqL1YF1kB7diLyZDZ6VCQ";
+    $.ajax({
+	    url : "http://api.wunderground.com/api/cbfd448ed7906dea/geolookup/conditions/q/" + pos + ".json",
+	    dataType : "jsonp",
+	    success : function(data) {
+			console.log(data);
 			$('#weather-data').append(
 				"<li>" + "City: " + data.location.city + "</li>" +
 				"<li>" + "Weather: " + data.current_observation.weather + "</li>" +
@@ -19,5 +32,12 @@ $(function(){
 				"<li>" + "Dewpoint: " + data.current_observation.dewpoint_string + "</li>"
 			);
 		}
-  });
-});
+	});
+    $('#staticmap').append("<img src='"+imsrc+"'>");//append img src to html div
+}
+
+// onError Callback receives a PositionError object
+function onError(error) {
+	alert('code: '    + error.code    + '\n' +
+          'message: ' + error.message + '\n');
+}
